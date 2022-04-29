e8fecd6de09e9e824824f3b0891f4e8f54be80b9
Wanqi-G <93185409+Wanqi-G@users.noreply.github.com>
2022-04-29 16:47:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employeerds.csmqnqay16nd.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "aws123456"
-customdb = "employeerds"
+customdb = "EmployeeRDS"
 custombucket = "gohwanqi-employee-bucket"
 customregion = "us-east-1"
